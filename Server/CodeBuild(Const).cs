611514c3bee241768cfc8d7973666b7291ce9743
28810 <28810@YEXIANGQIN>
2018-09-06 09:23:15 +0800

    升级 csredis 至 2.5.0

--- Server/CodeBuild(Const).cs
+++ Server/CodeBuild(Const).cs
@@ -423,7 +423,7 @@ public static partial class {0}ExtensionMethods {{
 	</PropertyGroup>
 	<ItemGroup>
 		<PackageReference Include=""dng.Mssql"" Version=""1.1.7"" />
-		<PackageReference Include=""CSRedisCore"" Version=""2.4.0"" />
+		<PackageReference Include=""CSRedisCore"" Version=""2.5.0"" />
 	</ItemGroup>
 </Project>
 ";
@@ -440,7 +440,7 @@ public static partial class {0}ExtensionMethods {{
 		<ProjectReference Include=""..\{0}.db\{0}.db.csproj"" />
 	</ItemGroup>
 	<ItemGroup>
-		<PackageReference Include=""Caching.CSRedis"" Version=""2.4.0"" />
+		<PackageReference Include=""Caching.CSRedis"" Version=""2.5.0"" />
 		<PackageReference Include=""Microsoft.AspNetCore.Mvc"" Version=""2.1.1"" />
 		<PackageReference Include=""Microsoft.AspNetCore.Session"" Version=""2.1.1"" />
 		<PackageReference Include=""Microsoft.AspNetCore.Diagnostics"" Version=""2.1.1"" />
